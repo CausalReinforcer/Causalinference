2af7eee2ec85d0cc70155604ad49972bac19a9aa
CausalReinforcer <flyupupup@gmail.com>
2017-11-29 20:36:34 -0800

    just start changing things

--- causalinference/estimators/base.py
+++ causalinference/estimators/base.py
@@ -19,20 +19,17 @@ class Estimator(Dict):
 		output = '\n'
 		output += 'Treatment Effect Estimates: ' + self._method + '\n\n'
 
-		entries1 = ['', 'Est.', 'S.e.', 'z', 'P>|z|',
-		           '[95% Conf. int.]']
+		entries1 = ['', 'Est.', 'S.e.', 'z', 'P>|z|', '[95% Conf. int.]']
 		entry_types1 = ['string']*6
 		col_spans1 = [1]*5 + [2]
-		output += tools.add_row(entries1, entry_types1,
-		                        col_spans1, table_width)
+		output += tools.add_row(entries1, entry_types1, col_spans1, table_width)
 		output += tools.add_line(table_width)
 
 		entry_types2 = ['string'] + ['float']*6
 		col_spans2 = [1]*7
 		for (name, coef, se) in zip(names, coefs, ses):
 			entries2 = tools.gen_reg_entries(name.upper(), coef, se)
-			output += tools.add_row(entries2, entry_types2,
-			                        col_spans2, table_width)
+			output += tools.add_row(entries2, entry_types2, col_spans2, table_width)
 
 		return output
 
@@ -48,11 +45,9 @@ class Estimators(Dict):
 
 		self._dict = {}
 
-
 	def __setitem__(self, key, item):
 
 		self._dict[key] = item
-
 
 	def __str__(self):
 
